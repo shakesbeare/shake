77a1945b2efe0dcac39e1b0bc06afebc871b45e8
Berint Moffett <berint.moffett@gmail.com>
2024-04-09 16:15:09 -0600

    feat: xtask install script

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -1,3 +1,5 @@
+use std::path::PathBuf;
+
 use clap::CommandFactory;
 use clap::Parser;
 
@@ -12,9 +14,23 @@ enum SubCommand {
     /// Generate the manpage
     #[clap(name = "manpage")]
     Manpage,
+    /// Install shake to the system
+    #[clap(name = "install")]
+    Install,
 }
 
 fn main() -> std::io::Result<()> {
+    let app = Cli::parse();
+
+    match app.subcmd {
+        SubCommand::Manpage => build_manpage()?,
+        SubCommand::Install => install()?,
+    }
+
+    Ok(())
+}
+
+fn build_manpage() -> std::io::Result<()> {
     let out_dir = std::path::PathBuf::from(
         std::env::var_os("OUT_DIR").ok_or(std::io::ErrorKind::NotFound)?,
     );
@@ -28,6 +44,42 @@ fn main() -> std::io::Result<()> {
         .spawn()
         .expect("failed to spawn gzip")
         .wait();
+
+    Ok(())
+}
+
+fn install() -> std::io::Result<()> {
+    use std::process::Command;
+    let built_man_dir = std::env::current_dir()?;
+    std::env::set_var("OUT_DIR", &built_man_dir);
+
+    let manpath = {
+        let os = std::env::consts::OS;
+        match os {
+            "macos" => PathBuf::from("/usr/local/share/man/man1"),
+            os => panic!("Unsupported OS, {}", os),
+        }
+    };
+
+    // 1) cargo install shake
+    // 2) build manpage
+    // 3) copy manpage to /usr/local/man/man1/
+    Command::new("cargo")
+        .args(vec!["install", "--path", "."])
+        .spawn()
+        .expect("failed to spawn `cargo install`")
+        .wait()?;
+    Command::new("cargo")
+        .args(vec!["xtask", "manpage"])
+        .spawn()
+        .expect("failed to spawn `cargo xtask manpage`")
+        .wait()?;
+    std::fs::copy(
+        built_man_dir.join("shake.1.gz"),
+        manpath.join("shake.1.gz"),
+    )?;
+
+    std::fs::remove_file(built_man_dir.join("shake.1.gz"))?;
 
     Ok(())
 }
