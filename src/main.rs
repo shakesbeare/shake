a18fb8f063a079d46e176d2f99ad46923a458984
Berint Moffett <berint.moffett@gmail.com>
2024-04-10 11:32:16 -0600

    print error messages from subcommands

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,27 @@
-use shake::*;
 use shake::create::*;
+use shake::*;
 
-use clap::Parser;
 use anyhow::Result;
+use clap::Parser;
 
 fn main() -> Result<()> {
     let app = Cli::parse();
 
-    match app.subcmd {
+    let app_result = match app.subcmd {
         SubCommand::Init { cargo, go, lfs } => init(cargo, go, lfs),
-        SubCommand::New { name, cargo, go, lfs } => new(name, cargo, go, lfs),
+        SubCommand::New {
+            name,
+            cargo,
+            go,
+            lfs,
+        } => new(name, cargo, go, lfs),
         SubCommand::Clone { uri, branch } => clone(uri, branch),
         SubCommand::Checkout { branch, b, force } => checkout(branch, b, force),
+    };
+
+    if let Err(e) = app_result {
+        eprintln!("{}", e);
     }
+
+    Ok(())
 }
